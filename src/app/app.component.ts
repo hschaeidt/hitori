336fcc6e404b2084ad6cab14fb11186475852b8b
Hendrik Schaeidt <he.schaeidt@gmail.com>
2017-09-12 18:03:09 +0200

    deactivate auth0 login for now
    * will be changed to email password based authentication with graphcool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,25 @@ import { Auth0Provider } from '../providers/auth0/auth0';
 export class MyApp {
   rootPage:any = PresentationPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, authProvider: Auth0Provider) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              public authProvider: Auth0Provider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+  }
 
-      // Auth handling
-      authProvider.handleAuthentication().then(() => {
-        // success
-      }, () => {
-        authProvider.isAuthenticated().then(isAuthenticated => {
-          if (!isAuthenticated) {
-            authProvider.login();
-          }
-        });
+  auth0LoginHandling() {
+    // Auth handling
+    this.authProvider.handleAuthentication().then(() => {
+      // success
+    }, () => {
+      this.authProvider.isAuthenticated().then(isAuthenticated => {
+        if (!isAuthenticated) {
+          this.authProvider.login();
+        }
       });
     });
   }
